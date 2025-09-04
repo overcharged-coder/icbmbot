95d661a4bba0ff4b941d4c9e80690334738b2f39
Madhumita16 <biotech4job@gmail.com>
2025-09-04 20:02:31 +0530

    bhai maths kor re bara

--- user_interface.py
+++ user_interface.py
@@ -44,12 +44,14 @@ EnumT = TypeVar('EnumT', bound=StrEnum)
 class User_Interface:
     async def main(self, commands: list[str], config_path: str, allow_upgrade: bool) -> None:
         self.config = Config.from_yaml(config_path)
+        print(f'{LOGO} • {self.config.version}', end='', flush=True)
 
         async with API(self.config) as self.api:
-            print(f'{LOGO} {self.config.version}\n')
-
             account = await self.api.get_account()
             username: str = account['username']
+
+            print(f' • {username}\n')
+
             self.api.append_user_agent(username)
             await self._handle_bot_status(account.get('title'), allow_upgrade)
             await self._test_engines()
